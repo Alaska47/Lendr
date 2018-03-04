a72e218e972751c12de8d82638e7a6007f7fc680
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2018-03-03 20:35:24 -0500

    Version with WilliamCharts

--- app/src/main/java/com/akotnana/lendr/activities/LoginActivity.java
+++ app/src/main/java/com/akotnana/lendr/activities/LoginActivity.java
@@ -2,22 +2,43 @@ package com.akotnana.lendr.activities;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.akotnana.lendr.R;
 import com.akotnana.lendr.activities.BaseActivity;
+import com.akotnana.lendr.utils.BackendUtils;
+import com.akotnana.lendr.utils.VolleyCallback;
+import com.android.volley.VolleyError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+
+import java.util.HashMap;
 
 import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 import br.com.simplepass.loading_button_lib.interfaces.OnAnimationEndListener;
 
 public class LoginActivity extends BaseActivity {
 
+    public static String TAG = "LoginActivity";
+
+    private FirebaseAuth mAuth;
+
     private CircularProgressButton btn;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +48,59 @@ public class LoginActivity extends BaseActivity {
         TextView logoTitle = findViewById(R.id.logo_title);
         logoTitle.setTypeface(fontManager.getTypeFaceRegular());
 
+        username = (EditText) findViewById(R.id.input_username);
+        password = (EditText) findViewById(R.id.input_password);
+
         btn = (CircularProgressButton) findViewById(R.id.sign_in);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 btn.startAnimation();
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent i = new Intent(LoginActivity.this, NavigationActivity.class);
-                        startActivity(i);
-                        btn.dispose();
-                        finish();
-                    }
-                }, 2000);
+                btn.setEnabled(false);
+                mAuth.signInWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseAuth.getInstance().getCurrentUser().getIdToken(true).addOnSuccessListener(new OnSuccessListener<GetTokenResult>() {
+                                        @Override
+                                        public void onSuccess(GetTokenResult result) {
+                                            Log.d("DataStorage", result.getToken());
+                                            final String idToken = result.getToken();
+                                            BackendUtils.doPostRequest("/api/v1/login", new HashMap<String, String>() {{
+                                                put("auth_token", idToken);
+                                            }}, new VolleyCallback() {
+                                                @Override
+                                                public void onSuccess(String result) {
+                                                    Log.d(TAG, result);
+                                                    btn.dispose();
+                                                }
 
-                //TODO firebase login, send the deets to rahul
+                                                @Override
+                                                public void onError(VolleyError error) {
+                                                    btn.revertAnimation();
+                                                }
+                                            }, getApplicationContext());
+                                        }
+                                    });
+                                    btn.setEnabled(true);
+                                    updateUI(mAuth.getCurrentUser());
+                                } else {
+                                    btn.revertAnimation();
+                                    btn.setEnabled(true);
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Failed to login...",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
             }
         });
 
@@ -59,6 +115,18 @@ public class LoginActivity extends BaseActivity {
                 finish();
             }
         });
+    }
+
+    public void updateUI(FirebaseUser currUser) {
+        if(currUser != null) {
+            Intent intent = new Intent(getApplicationContext(), NavigationActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        } else {
+            btn.revertAnimation();
+            Toast.makeText(this, "Failed login...", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
--- app/src/main/java/com/akotnana/lendr/activities/SignInActivity.java
+++ app/src/main/java/com/akotnana/lendr/activities/SignInActivity.java
@@ -4,13 +4,30 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.akotnana.lendr.R;
+import com.akotnana.lendr.utils.BackendUtils;
+import com.akotnana.lendr.utils.DataStorage;
+import com.akotnana.lendr.utils.VolleyCallback;
+import com.android.volley.VolleyError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
 
 import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 import br.com.simplepass.loading_button_lib.interfaces.OnAnimationEndListener;
@@ -20,6 +37,8 @@ import xyz.belvi.luhn.interfaces.LuhnCallback;
 import xyz.belvi.luhn.interfaces.LuhnCardVerifier;
 
 public class SignInActivity extends BaseActivity {
+
+    public static String TAG = "SignInActivity";
 
     private FirebaseAuth mAuth;
     private CircularProgressButton btn;
@@ -44,22 +63,68 @@ public class SignInActivity extends BaseActivity {
 
         btn = (CircularProgressButton) findViewById(R.id.sign_in);
 
+        final String token = FirebaseInstanceId.getInstance().getToken();
+        String realToken = "";
+        if(token.equals("")) {
+            realToken = new DataStorage(getApplicationContext()).getData("firebaseID");
+        } else {
+            realToken = token;
+        }
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 btn.startAnimation();
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent i = new Intent(SignInActivity.this, NavigationActivity.class);
-                        startActivity(i);
-                        btn.dispose();
-                        finish();
-                    }
-                }, 2000);
+                btn.setEnabled(false);
 
-                //TODO firebase login, send the deets to rahul
+                mAuth.createUserWithEmailAndPassword(username.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                            .setDisplayName(username.getText().toString())
+                                            .build();
+                                    user.updateProfile(profileUpdates);
+                                    FirebaseAuth.getInstance().getCurrentUser().getIdToken(true).addOnSuccessListener(new OnSuccessListener<GetTokenResult>() {
+                                        @Override
+                                        public void onSuccess(GetTokenResult result) {
+                                            Log.d("DataStorage", result.getToken());
+                                            final String idToken = result.getToken();
+                                            BackendUtils.doPostRequest("/api/v1/register", new HashMap<String, String>() {{
+                                                put("auth_token", idToken);
+                                            }}, new VolleyCallback() {
+                                                @Override
+                                                public void onSuccess(String result) {
+                                                    Log.d(TAG, result);
+                                                    btn.dispose();
+                                                }
+
+                                                @Override
+                                                public void onError(VolleyError error) {
+                                                    btn.revertAnimation();
+                                                }
+                                            }, getApplicationContext());
+                                        }
+                                    });
+                                    btn.setEnabled(true);
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    btn.revertAnimation();
+                                    btn.setEnabled(true);
+                                    Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
             }
         });
 
@@ -107,5 +172,23 @@ public class SignInActivity extends BaseActivity {
                 finish();
             }
         });
+    }
+
+    public void updateUI(FirebaseUser currUser) {
+        if(currUser != null) {
+            Intent intent = new Intent(getApplicationContext(), NavigationActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        } else {
+            btn.revertAnimation();
+            Toast.makeText(this, "Failed to sign up...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        btn.dispose();
     }
 }
